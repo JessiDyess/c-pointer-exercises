15bb988f224e27def7c47f43d975cf3f994266ab
JessiDyess <47078312+JessiDyess@users.noreply.github.com>
2019-03-16 23:52:48 -0600

    Update monolithic_array.c

--- monolithic_array.c
+++ monolithic_array.c
@@ -7,6 +7,21 @@
 void run_monolithic_array() {
     printf("Running monolithic array\n");
 
-    // YOUR CODE HERE
+   //define a struct
+struct KeyValue{
+    char key;
+    int value;
+};
+
+//create and initialize dynamic array
+void declareArr(){
+    KeyValue *Array = malloc(10 * sizeof *Array);
+    for (int i=0; i <= 10; ++i) {
+        struct KeyValue newValue ={"a", 0};
+        Array[i] = newValue;
+    }
+    free(Array);
+    
+}
 
 }
