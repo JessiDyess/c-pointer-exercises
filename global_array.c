d7f089d55b5c7bcb269e23d4cbded4b433ce5beb
JessiDyess <47078312+JessiDyess@users.noreply.github.com>
2019-03-16 23:43:48 -0600

    Update global_array.c

--- global_array.c
+++ global_array.c
@@ -7,6 +7,26 @@
 void run_global_array() {
     printf("Running global array\n");
 
-    // YOUR CODE HERE
+    //define a struct
+struct KeyValue{
+    char key;
+    int value;
+};
 
+//create static array of structs
+static struct KeyValue StaticArray[10];
+    
+int main() {
+    for (int i=0; i <= 10; ++i) {
+       struct KeyValue newValue ={"a", 0};
+       StaticArray[i] = newValue;
+    }
+
+    for(int x =0; x <= 10; x++){
+        printf("Here are your numbers %d\n", StaticArray[x].value);
+        printf("Here are you characters %d\n", StaticArray[x].key);
+    }
+
+    return 0;
+}
 }
