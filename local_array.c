58df28ba080267eb8b70616f63c4cb4e6e27a195
JessiDyess <47078312+JessiDyess@users.noreply.github.com>
2019-03-16 23:47:47 -0600

    Update local_array.c

--- local_array.c
+++ local_array.c
@@ -7,6 +7,20 @@
 void run_local_array() {
     printf("Running local array\n");
 
-    // YOUR CODE HERE
+   //define a struct
+struct KeyValue{
+    char key;
+    int value;
+}
+
+//create static array of structs inside function
+void declareArr(){
+    static struct KeyValue StaticArray[10];
+    for (int i=0; i <= 10; ++i) {
+        struct KeyValue newValue ={"a", 0};
+        StaticArray[i] = newValue;
+    }
+} 
+
 
 }
