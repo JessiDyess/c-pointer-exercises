a394ba163e7d6dd219c6c06abaccf1efeb37e5df
JessiDyess <47078312+JessiDyess@users.noreply.github.com>
2019-03-16 23:59:06 -0600

    Update array_of_pointers.c

--- array_of_pointers.c
+++ array_of_pointers.c
@@ -6,6 +6,24 @@
 void run_array_of_pointers() {
     printf("Running array of pointers\n");
 
-    // YOUR CODE HERE
+  //define a struct
+struct KeyValue{
+    char key;
+    int value;
+};
+
+//create array
+void declareArr(){
+    struct KeyValue **array;
+    *array = (struct KeyValue *)malloc(10 * sizeof (*array));
+    array = malloc(sizeof(int *) * 10);
+
+    for (int i=0; i <= 10; ++i) {
+        struct KeyValue newValue ={"a", 0};
+        array[i] = newValue;
+    }
+    free(array);
+
+}
 
 }
